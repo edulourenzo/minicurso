80d56b7712ef3d77707ba97af4d3f28fe2d22c36
edulourenzo <edulourenzo@hotmail.com>
2022-01-14 17:44:25 -0300

    Corrigir html e adicionar estilo

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const nomeDoPersonagem = document.querySelector('#especie');
-const nomeDoPersonagem = document.querySelector('#status');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/826`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'aplication/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        
+
+    });
+}
+
+botao.onclick = pegarPersonagem;
